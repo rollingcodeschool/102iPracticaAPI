375bae6fbe320ebe89c7834beab8b981fdc5b5ef
Emilse Arias <emilsearias@gmail.com>
2024-09-04 11:50:49 -0300

    cambie la vista de la imagen del personaje y acomodé el color de fondo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import Frase from "./components/Frase"
 import logo from './assets/logosimpson.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { useEffect, useState } from "react";
+
+
 function App() {
+  const [frasePersonaje, setFrasePersonaje] = useState({})
+
+  useEffect(()=>{
+    consultarAPI();
+  },[])
+
+  const consultarAPI = async() =>{
+    console.log('hola mundo')
+    //enviar una solicitud get
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const datos = await respuesta.json();
+    console.log(datos[0])
+    //respuesta
+    //almacenar la respuesta en el state
+    setFrasePersonaje(datos[0])
+  }
   
   return (
     <Container className="text-center my-5">
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -2,17 +2,19 @@ import { Card } from "react-bootstrap";
 
 const Frase = () => {
   return (
-    <Card className="text-center">
-      <Card.Img
-        variant="top"
+    <section>
+      <img
         src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
-        className="w-50"
+        alt="personaje"
+        className="character mb-4"
       />
-      <Card.Body >
-        <Card.Title>Personaje</Card.Title>
-        <Card.Text>Frase</Card.Text>
-      </Card.Body>
-    </Card>
+      <Card className="text-center">
+        <Card.Body>
+          <Card.Title>Personaje</Card.Title>
+          <Card.Text>Frase</Card.Text>
+        </Card.Body>
+      </Card>
+    </section>
   );
 };
 
