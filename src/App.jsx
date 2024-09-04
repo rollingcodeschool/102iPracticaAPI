c0df3b720670f49218b6cf4ab7b2691d6325858e
Emilse Arias <emilsearias@gmail.com>
2024-09-04 11:04:12 -0300

    agregamos una card con la imagen de nelson, logo y boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { Button, Container } from "react-bootstrap"
+import Frase from "./components/Frase"
+import logo from './assets/logosimpson.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 function App() {
   
   return (
-    <>
-   
-    </>
+    <Container className="text-center my-5">
+      <img src={logo} alt="Logo simpson" className="w-50 mb-4"/>
+      <Frase></Frase>
+      <Button variant="warning" className="mt-4">Enviar</Button>
+    </Container>
   )
 }
 
---
+++ src/components/Frase.jsx
@@ -0,0 +1,19 @@
+import { Card } from "react-bootstrap";
+
+const Frase = () => {
+  return (
+    <Card className="text-center">
+      <Card.Img
+        variant="top"
+        src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+        className="w-50"
+      />
+      <Card.Body >
+        <Card.Title>Personaje</Card.Title>
+        <Card.Text>Frase</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Frase;
