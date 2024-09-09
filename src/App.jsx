e6495c69a335ce53298381c97cc2c8feca0f6cdb
Emilse Arias <emilsearias@gmail.com>
2024-09-09 10:39:23 -0300

    mostramos el spinner como componente condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,53 @@
-import { Button, Container } from "react-bootstrap"
-import Frase from "./components/Frase"
-import logo from './assets/logosimpson.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import { Button, Container, Spinner } from "react-bootstrap";
+import Frase from "./components/Frase";
+import logo from "./assets/logosimpson.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { useEffect, useState } from "react";
 
-
 function App() {
-  const [frasePersonaje, setFrasePersonaje] = useState({})
+  const [frasePersonaje, setFrasePersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     consultarAPI();
-  },[])
+  }, []);
 
-  const consultarAPI = async() =>{
-    try{
-      console.log('hola mundo')
+  const consultarAPI = async () => {
+    try {
+      setMostrarSpinner(true)
+      console.log("hola mundo");
       //enviar una solicitud get
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
       const datos = await respuesta.json();
-      console.log(respuesta)
-      if(respuesta.status === 200){
-        setFrasePersonaje(datos[0])
+      console.log(respuesta);
+      if (respuesta.status === 200) {
+        setFrasePersonaje(datos[0]);
+        setMostrarSpinner(false)
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
       //agregar un mensaje final para el usuario
     }
-    
-  }
-  
+  };
+
   return (
     <Container className="text-center my-5">
-      <img src={logo} alt="Logo simpson" className="w-50 mb-4"/>
-      <Frase frasePersonaje={frasePersonaje}></Frase>
-      <Button variant="warning" className="mt-4" onClick={consultarAPI}>Enviar</Button>
+      <img src={logo} alt="Logo simpson" className="w-50 mb-4" />
+      {mostrarSpinner ? (
+        <div>
+          <Spinner animation="grow" variant="info" />
+        </div>
+      ) : (
+        <Frase frasePersonaje={frasePersonaje}></Frase>
+      )}
+      <Button variant="warning" className="mt-4" onClick={consultarAPI}>
+        Enviar
+      </Button>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
